0a42871a21dc9292f84672c850e4ccee8d937534
Belkahla <98918742+7louu@users.noreply.github.com>
2024-05-14 22:08:41 +0100

    Finished the dialogue Manager and starting to add the dialogue and the options to the game

--- dialogueManager/src/dialogueManager.java
+++ dialogueManager/src/dialogueManager.java
@@ -3,11 +3,42 @@ import java.util.List;
 class dialaogueNode{
     private String text;
     private List<dialaogueNode> Options;
-    public dialaogueNode(String text,List<dialaogueNode> Options){
+    public dialaogueNode(String text){
         this.text = text;
-        this.Options = Options;
+        this.Options = new ArrayList<>();
+    }
+    public void addOption(dialaogueNode option){
+        Options.add(option);
+    }
+    public void displayText(){
+        System.out.println(text);
+    }
+    public void displayOptions()
+    {
+        for (int i=0; i < Options.size(); i++)
+        {
+            System.out.println((i + 1) + " . " + Options.get(i).text);
+        }
     }
 }
 public class dialogueManager{
+    private dialaogueNode root;
+    public dialogueManager(String rootText)
+    {
+        this.root = new dialaogueNode(rootText);
+    }
+    public void addoptionToRoot(dialaogueNode option)
+    {
+        root.addOption(option);
+    }
+    public void traverseDialogueTree(dialaogueNode node)
+    {
+        node.displayText();
+        node.displayOptions();
+    }
+    public void startConversation()
+    {
+        traverseDialogueTree(root);
+    }
 
 }
